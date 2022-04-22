7ea7e6cacef14a45b001241bb678cf1c3d3cde0d
Alexander Kumanov <kumana@abv.bg>
2022-04-22 17:31:46 +0300

    added distance-converter assignment

--- dom-events-exercise/05. JS-Advanced-DOM-Events-Exercise-Resources/07. Distance-Converter/distanceConverter.js
+++ dom-events-exercise/05. JS-Advanced-DOM-Events-Exercise-Resources/07. Distance-Converter/distanceConverter.js
@@ -3,18 +3,33 @@ function attachEventsListeners() {
     let convertFrom = document.querySelector('select[id="inputUnits"]');
     let convertTo = document.querySelector('select[id="outputUnits"]');
     let convertBtn = document.querySelector('input[id="convert"]');
+    let outputDistance = document.querySelector('input[id="outputDistance"]');
     convertBtn.addEventListener('click', convertParams)
 
-    function convertParams(event) {
+    function convertParams() {
 
-        console.log(`Convert ${inputDistance.value} ${convertFrom.value} to ${convertTo.value}`);
-        console.log(convertToMeters(inputDistance.value)[convertTo.value]);
+        let meters = convertToMeters(Number(inputDistance.value))[convertFrom.value];
+        let result = convertFromMeters(meters)[convertTo.value];
+        outputDistance.value = result;
     }
 
     function convertToMeters(value) {
         return {
+            km: value * 1000,
+            m: value * 1,
+            cm: value * 0.01,
+            mm: value * 0.001,
+            mi: value * 1609.34,
+            yrd: value * 0.9144,
+            ft: value * 0.3048,
+            in: value * 0.0254,
+        }
+    }
+
+    function convertFromMeters(value) {
+        return {
             km: value / 1000,
-            m: value,
+            m: value / 1,
             cm: value / 0.01,
             mm: value / 0.001,
             mi: value / 1609.34,
@@ -23,4 +38,5 @@ function attachEventsListeners() {
             in: value / 0.0254,
         }
     }
+
 }
